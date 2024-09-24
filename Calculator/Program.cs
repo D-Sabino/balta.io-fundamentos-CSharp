a7ba4147ddbd0c65a2792fe09696ca0f6a0e8934
Daniel Sabino <40477679+D-Sabino@users.noreply.github.com>
2024-09-24 06:31:16 -0300

    Mão na massa: Criando uma calculadora
    Aula 02: Soma.

---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Primeiro valor: ");
+            float valor_1 = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Segundo valor: ");
+            float valor_2 = float.Parse(Console.ReadLine());
+
+            float resultado = valor_1 + valor_2;
+            Console.WriteLine("");
+            Console.WriteLine("A soma é " + resultado);
+        }
+    }
+}
