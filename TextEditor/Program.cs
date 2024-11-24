1dfe8f8fa483e768f33ef543bc89bd8016d821a0
Daniel Sabino <40477679+D-Sabino@users.noreply.github.com>
2024-11-24 07:28:48 -0300

    Editor de textos: Abrir, Editar e Salvar

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -23,7 +23,18 @@ namespace TextEditor{
         }
 
         static void Abrir(){
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo?");
+            string path = Console.ReadLine();
 
+            using(var file = new StreamReader(path)){
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
         }
 
         static void Editar(){
@@ -32,9 +43,29 @@ namespace TextEditor{
             Console.Clear();
             Console.WriteLine("Digite seu texto abaixo: (ESC para sair)");
             
-            while(Console.ReadKey().Key != ConsoleKey.Escape){
-                
+            do{
+                text += Console.ReadLine();
+                text += Environment.NewLine;
             }
+            while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Salvar(text);
+
+        }
+
+        //Modelo básico para salvar o arquivo.
+        static void Salvar(string text){
+            Console.Clear();
+            Console.WriteLine("Qual o caminho para salvar o arquivo?");
+            var path = Console.ReadLine();
+
+            using(var file = new StreamWriter(path)){
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            Console.ReadLine();
+            Menu();
 
         }
 
