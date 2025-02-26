d52edd5dfc5463a21876bcdd8bea0f7c116d458f
Daniel Sabino <40477679+D-Sabino@users.noreply.github.com>
2025-02-26 06:33:54 -0300

    Finalização do curso.

--- MeuApp/obj/Debug/net8.0/MeuApp.AssemblyInfo.cs
+++ MeuApp/obj/Debug/net8.0/MeuApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dee43efb3a08b0166ee7e1f619220d1ad8c9ef48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da8286d9dbd0f430fdec19a165fd1ebf1b1dae7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
